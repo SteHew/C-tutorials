f4bff9b9550254c7b454e7b0e6a373f88538a100
Stephen Hewlett <10000203@student.toiohomai.ac.nz>
2018-03-02 16:05:53 +1300

    Up to Exercise 10 of tutorials

--- COMP6211exercises/COMP6211exercises/Program.cs
+++ COMP6211exercises/COMP6211exercises/Program.cs
@@ -20,6 +20,7 @@ namespace COMP6211exercises
 
             Console.ReadLine();
             */
+
             /*
             // Exercise 2 - Addition
 
@@ -74,6 +75,7 @@ namespace COMP6211exercises
                 Console.WriteLine("{0} x {1} = {2}", anyNumber, i, anyNumber * i);
             }
             Console.WriteLine();
+            Console.ReadLine();
 
 
             // Exercise 6 - circle area and perimeter
@@ -87,7 +89,7 @@ namespace COMP6211exercises
             Console.WriteLine("A circle with radius 7.5 has an area of {0} and a perimeter of {1}.", 
                 area, perim);
             Console.WriteLine();
-            */
+            Console.ReadLine();
 
 
             // Exercise 7 - swap two variables
@@ -108,14 +110,68 @@ namespace COMP6211exercises
 
             char[] charArray = variables.ToCharArray();
             Array.Reverse(charArray);
-            /*
-            foreach (char variablesChar in charArray)
-            {
-                Console.Write(variablesChar);
-            }
-            */
-            Console.Write("The variables are now: ");
+            
+            //foreach (char variablesChar in charArray)
+            //{
+            //    Console.Write(variablesChar);
+            //}
+            
+            Console.Write("The order of the variables is now: ");
             Console.WriteLine(charArray);
+            Console.ReadLine();
+
+
+            // Exercise 8 - user defined number is positive or negative
+
+            Console.WriteLine();
+            Console.Write("Please type any number and press enter to find out if " +
+                "it is a positive or a negative number: ");
+            float userNumber = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (userNumber > 0) Console.WriteLine("It is a positive number.");
+
+            else Console.WriteLine("It is a negative number.");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            
+            // Exercise 9 - the greatest of 3 numbers
+
+            Console.WriteLine();
+            Console.Write("Please type a number and press enter: ");
+            float Num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Please type another number and press enter: ");
+            float Num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Please type a third number and press enter: ");
+            float Num3 = float.Parse(Console.ReadLine());
+
+            if (Num1 > Num2 & Num1 > Num3) Console.WriteLine("{0} is the greatest of the 3 numbers.", Num1);
+            Console.WriteLine();
+            if (Num2 > Num1 & Num2 > Num3) Console.WriteLine("{0} is the greatest of the 3 numbers.", Num2);
+            else Console.WriteLine("{0} is the greatest of the 3 numbers.", Num3);
+            Console.WriteLine();
+            Console.ReadLine();
+            */
+
+            // Exercise 10 - one dimensional array populated with random numbers
+
+            Console.WriteLine();
+            Console.WriteLine("Here are 5 random numbers between 0 and 500: ");
+            Console.WriteLine();
+
+            int[] array1 = new int[5];
+            Random randNums = new Random();
+            for (int i = 0; i <= 4; i++)
+            {
+                array1[i] = randNums.Next(1, 501);
+                Console.WriteLine(array1[i]);
+            }
+            Console.WriteLine();
             Console.ReadLine();
         }
     }
---
+++ COMP6211exercises/RandomNumbers/RandomNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Here are 5 random numbers from 1 to 500: ");
+            Console.WriteLine();
+
+            int[] array1 = new int[5];
+            Random randNums = new Random();
+            for (int i = 0; i <= 4; i++)
+            {
+                array1 [i] = randNums.Next(1, 501);
+                Console.WriteLine(array1[i]);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
